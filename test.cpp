b3c9aa5498bb120a9040c1ca3f03e0bd7354dc07
Fletcher Karl Robinon <fkrobinson@mac.com>
2021-04-26 16:01:17 -0700

    test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Objects/box.h"
+
+using namespace std;
+
+/**
+ * Calculate and print the volume of a box.
+ */
+int main()
+{
+    printf ( "Hello Kitty" );
+    box package{ 10, 10, 10 };
+    cout << "Package length: " << package.length << endl;
+    cout << "Package width:  " << package.width << endl;
+    cout << "Package height: " << package.height << endl;
+    cout << "Package volume: " << package.volume() << endl;
+}
